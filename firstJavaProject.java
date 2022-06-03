5ed40f25eba37ba9b7da1d9fb6e906ad3b77abcf
Arulkumaran <arulkumaran0108@gmail.com>
2022-06-04 03:29:47 +0800

    Add files via upload

---
+++ firstJavaProject.java
@@ -0,0 +1,12 @@
+
+public class firstJavaProject {
+
+	public static void main(String[] args) {
+		// The code below prints text to the screen
+	System.out.println("Hello World");
+	System.out.print("Arul");
+	System.out.print("kumaran");
+	
+	}
+
+}
